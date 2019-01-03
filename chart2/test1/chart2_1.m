eafff6f3ea30aac5f730eb0519712a0719af43b4
Jason52 <1479293909@qq.com>
2019-01-03 20:18:07 +0800

    add list

--- chart2/test1/chart2_1.m
+++ chart2/test1/chart2_1.m
@@ -1,9 +1,11 @@
-P_init=0.01;
-p=(1:10);
-count=(1:10);
-p(1)=P_init;
-for i=1:9
+P_init=0.01;                    %初始传感器置信度
+N = 10;  
+p=(1:N+1);                       %定义每次概率值
+p(1)=P_init;                    %传入初值
+for i=1:N
 p(i+1)=p(i)/(p(i)+(1-p(i))/3);
 end
 
-plot(count,p,'b-o');
+plot(p,'b-o');
+
+
